af74b2d50334fe092a8716e91489243444fdf60d
cwbak <chanwoong.bak@gmail.com>
2026-08-27 18:13:04 +0900

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     proxy: {
       "/admin": {
         changeOrigin: true,
-        target: "http://127.0.0.1:8080",
+        target: "http://127.0.0.1:8081",
       },
     },
   },
